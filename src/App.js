6a7bb475a408f22ff2d9c4e35827e5e270d54624
Mark Bean <mbean0302@gmail.com>
2023-02-19 22:38:41 -0500

    End of day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      query: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.query);
+    });
+    console.log(filteredMonsters);
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => {
+            const query = e.target.value.toLowerCase();
+            this.setState(() => {
+              return { query };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster, i) => {
+          return (
+            <div key={"monster" + i}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
